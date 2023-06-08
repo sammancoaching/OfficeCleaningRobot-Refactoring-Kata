20738abe550daa31965b31e8fdbd6bd793d2133f
emily <emily@bacheconsulting.com>
2023-06-08 16:34:08 +0200

    Add version 3

---
+++ csharp/MarsRover/MarsRover3/LineDirection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover3
+{
+    enum LineDirection
+    {
+        H, V
+    }
+}
---
+++ csharp/MarsRover/MarsRover3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MarsRover3;
+
+using con = System.Console;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        int movements = Convert.ToInt32(con.ReadLine());
+        string[] coords = con.ReadLine().Split(' ');
+        var rob = new Rover(Convert.ToInt32(coords[0]), Convert.ToInt32(coords[1]));
+        for (int i = 0; i < movements; i++)
+        {
+            string[] movement = con.ReadLine().Split(' ');
+            Direction dir = (Direction)Enum.Parse(typeof(Direction), movement[0], true);
+            int steps = Convert.ToInt32(movement[1]);
+            rob.Move(dir, steps);
+        }
+        con.WriteLine("=> Visited: " + rob.Counter);
+    }
+}

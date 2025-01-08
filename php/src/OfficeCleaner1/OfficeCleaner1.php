e1e64d8b306bd4207482e2c3289ed4ef3cd255a4
mpeinhopf <max.peinhopf@gmail.com>
2025-01-08 23:36:41 +0100

    require autoloader

--- php/src/OfficeCleaner1/OfficeCleaner1.php
+++ php/src/OfficeCleaner1/OfficeCleaner1.php
@@ -2,5 +2,8 @@
 
 namespace OfficeCleaner1;
 
-if (PHP_SAPI === 'cli') { Program::main(STDIN); }
+if (PHP_SAPI === 'cli') { 
+  require_once __DIR__ . '/../../vendor/autoload.php';
+  Program::main(STDIN);
+}
 ?>
